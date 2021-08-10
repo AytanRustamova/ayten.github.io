4e6d4a42e694a5353a85ec63f25941ebe4cded2e
AytanRustamova <aytn1973@gmail.com>
2021-08-10 16:11:19 +0300

    some responsive problems solved

--- js/main.js
+++ js/main.js
@@ -41,21 +41,28 @@ window.addEventListener("scroll" , () => {
         aboutImg.style.transform= 'translateX(0%)';
         
     },1000)
-
-   
     
 });
 
 
-
-
-
 let burgerMenuLine = document.querySelector('.burger-menu .line');
+let bulgerMenuLineLi = document.querySelector('.burger-menu .line ul li');
 let toggleMenu = document.querySelector('.toggle-menu')
-
-burgerMenuLine.addEventListener('click',function(){
-    toggleMenu.style.display = 'block';
-    document.querySelector("body").style.background='rgba(0, 0, 0, 0.7)'
+let menuStatus= true;
+burgerMenuLine.addEventListener('click',() =>{
+   
+    if(menuStatus){
+        toggleMenu.style.display = 'block';
+        document.querySelector("body").style.background='rgba(255, 255, 255, 0.1)';
+        document.querySelector("body").style.opacity='1';
+        bulgerMenuLineLi.style.background = '#121212';
+        menuStatus=false;
+    } else{
+        console.log('salam da salam')
+        toggleMenu.style.display = 'none';
+        document.querySelector("body").style.background='rgba(0, 0, 0, 1)';
+        menuStatus=true;
+    }
 
 })
 
