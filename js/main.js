5c43170d43733e1a39e4d18f3a2feaabcd8add4b
AytanRustamova <aytn1973@gmail.com>
2021-08-10 20:11:01 +0300

    works section animation

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function mySlide(){
 
 window.addEventListener("scroll" , () => {
     let stickyHeader = document.querySelector("header");
-    console.log(stickyHeader);
+    // console.log(stickyHeader);
 
     stickyHeader.classList.toggle("sticky", window.scrollY>450);
 });
@@ -29,24 +29,50 @@ window.addEventListener("scroll" , () => {
 
 
 
-let aboutImg=document.querySelector('.about-grid img');
-let aboutContent=document.querySelector('.about-content');
-
-// let header=document.querySelector('.slide h1')
-// let desc=document.querySelector('.slide p')
 
 window.addEventListener("scroll" , () => {
+
     
-    setTimeout(()=>{
-        aboutImg.style.transform= 'translateX(0%)';
-        
-    },1000)
+let aboutImg=document.querySelector('.about-grid img');
+let aboutImgPosition = aboutImg.getBoundingClientRect().top;
+let screenPosition = window.innerHeight;
+    if(aboutImgPosition < screenPosition){
+        aboutImg.style.transform= 'translateX(0%)';    
+    }
     
 });
 
 
+  
+
+
+window.addEventListener("scroll" , () => {
+    let leftImg=document.querySelector('.leftImage');
+    let leftImgPosition = leftImg.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    
+    if(leftImgPosition < screenPosition){
+
+        leftImg.style.transform= 'translateX(0%)';
+    }
+});
+
+window.addEventListener("scroll" , () => {
+    
+    let rightImg=document.querySelector('.rightImage');
+    let rightImgPosition = rightImg.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    
+    if(rightImgPosition < screenPosition){
+        rightImg.style.transform= 'translateX(0%)';
+    }
+});
+
+
+
 let burgerMenuLine = document.querySelector('.burger-menu .line');
 let bulgerMenuLineLi = document.querySelector('.burger-menu .line ul li');
+let bulgerMenuLineUl = document.querySelector('.line ul');
 let toggleMenu = document.querySelector('.toggle-menu')
 let menuStatus= true;
 burgerMenuLine.addEventListener('click',() =>{
@@ -56,6 +82,8 @@ burgerMenuLine.addEventListener('click',() =>{
         document.querySelector("body").style.background='rgba(255, 255, 255, 0.1)';
         document.querySelector("body").style.opacity='1';
         bulgerMenuLineLi.style.background = '#121212';
+        
+
         menuStatus=false;
     } else{
         console.log('salam da salam')
